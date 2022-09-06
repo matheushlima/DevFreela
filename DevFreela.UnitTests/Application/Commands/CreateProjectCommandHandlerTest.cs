648315e90eb3ea260c9b824d8438ea670f7473ae
Matheus Lima <math.henry04@hotmail.com>
2022-09-05 23:19:46 -0300

    Implementação de Testes utilizando Mock

---
+++ DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTest.cs
@@ -0,0 +1,42 @@
+﻿using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Commands
+{
+    public class CreateProjectCommandHandlerTest
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnProjectId()
+        {
+            //Arrange
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+
+            var createProjectCommand = new CreateProjectCommand()
+            {
+                Title = "Titulo de Teste",
+                Description = "Descrição de teste",
+                IdClient = 1,
+                IdFreelancer = 2,
+                TotalCost = 50000
+            };
+
+            var createProjectCommandHandler = new CreateProjectCommandHandler(projectRepositoryMock.Object);
+
+            //Act
+            var id = await createProjectCommandHandler.Handle(createProjectCommand, new System.Threading.CancellationToken());
+
+            //Assert
+            Assert.True(id >= 0);
+            
+            projectRepositoryMock.Verify(pr => pr.AddAsync(It.IsAny<Project>()), Times.Once);
+        } 
+    }
+}
---
+++ DevFreela.UnitTests/Application/Queries/GetAllProjectsQueryHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Queries
+{
+    public class GetAllProjectsQueryHandlerTests
+    {
+        [Fact]
+        public async Task ThreeProjectsExist_Executed_ReturnThreeProjectViewModels()
+        {
+            //AAA
+            //Arrange
+            var projects = new List<Project>
+            {
+                new Project("Meu teste 1", "Descrição do teste", 1, 2, 10000),
+                new Project("Meu teste 2", "Descrição do teste", 2, 3, 20000),
+                new Project("Meu teste 3", "Descrição do teste", 3, 4, 30000)
+            };
+
+
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+            projectRepositoryMock.Setup(pr => pr.GetAllAsync().Result).Returns(projects);
+
+            var getAllProjectQuery = new GetAllProjectsQuery("");
+            var getAllProjectQueyHandler = new GetAllProjectsQueryHandler(projectRepositoryMock.Object);
+
+            //Act
+            var projectViewModelList = await getAllProjectQueyHandler.Handle(getAllProjectQuery, new CancellationToken());
+
+            //Assert
+            Assert.NotNull(projectViewModelList);
+            Assert.NotEmpty(projectViewModelList);
+            Assert.Equal(projects.Count, projectViewModelList.Count);
+
+            projectRepositoryMock.Verify(pr => pr.GetAllAsync().Result, Times.Once);
+
+        }
+    }
+}
